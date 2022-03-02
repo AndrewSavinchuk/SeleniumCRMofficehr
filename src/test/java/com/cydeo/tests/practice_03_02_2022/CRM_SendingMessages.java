2aaa022a62f498228cc02346909a73b64600c201
Aysun120 <aysundamar114@gmail.com>
2022-03-02 13:50:42 -0600

    CRMSending messages classes added

---
+++ src/test/java/com/cydeo/tests/practice_03_02_2022/CRM_SendingMessages.java
@@ -0,0 +1,64 @@
+package com.cydeo.tests.practice_03_02_2022;
+
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.CRM_Utilities;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class CRM_SendingMessages {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+
+        String browserType = ConfigurationReader.getProperty("browser");
+
+        driver = WebDriverFactory.getDriver(browserType);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        driver.get(ConfigurationReader.getProperty("env"));
+
+        CRM_Utilities.crm_login(driver);
+
+    }
+
+    @Test
+    public void sendingMessagesTest(){
+
+//        Users are on the homepage
+//        Users click the MESSAGE tab
+       driver.findElement(By.id("feed-add-post-form-tab-message")).click();
+
+//        Users write test message
+        driver.switchTo().frame(driver.findElement(By.cssSelector(".bx-editor-iframe")));
+        WebElement msgFrame=driver.findElement(By.tagName("body"));
+        msgFrame.sendKeys("Hello!");
+
+//        Users click the SEND button
+        driver.switchTo().defaultContent();
+        driver.findElement(By.id("blog-submit-button-save")).click();
+
+//        Verify the message is displayed on the feed
+        WebElement feed= driver.findElement(By.xpath("//div[starts-with(@id,'blog_post_body')]"));
+        Assert.assertEquals(feed.getText(),"Hello!","Message did not appear!");
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        BrowserUtils.sleep(3);
+        driver.quit();
+    }
+
+}
